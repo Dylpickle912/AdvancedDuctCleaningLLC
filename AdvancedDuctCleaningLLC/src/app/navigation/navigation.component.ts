3afa9f38c47b29a527e337e66e83fadd43c77771
dylpickle912 <dylpickle912@yahoo.com>
2023-04-26 15:58:33 -0400

    Made navigation mobile

--- AdvancedDuctCleaningLLC/src/app/navigation/navigation.component.ts
+++ AdvancedDuctCleaningLLC/src/app/navigation/navigation.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  public showMenu = false;
+  public showOurCompanyMenu = false;
+  public isMobile = false;
 
+  public ngOnInit() {
+    this.setIsMobile(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const width = event.target.innerWidth;
+    this.setIsMobile(width);
+  }
+
+  public setIsMobile(size: number): void {
+    this.isMobile = size <= 960;
+
+    if (this.showMenu && !this.isMobile) {
+      this.showMenu = false;
+    }
+  }
+
+  public toggleShowMenu(): void {
+    this.showMenu = !this.showMenu;
+
+    if (!this.showMenu) {
+      this.showOurCompanyMenu = false;
+    }
+  }
+
+  public toggleShowOurCompanyMenu(): void {
+    this.showOurCompanyMenu = !this.showOurCompanyMenu;
+  }
 }
