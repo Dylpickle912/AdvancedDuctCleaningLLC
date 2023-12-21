eead3c962ca2d64f07469f267778796565505de7
dylpickle912 <dylpickle912@yahoo.com>
2023-12-21 12:38:19 -0500

    Fixed meta tags

--- AdvancedDuctCleaningLLC/src/app/pages/meet-our-owner/meet-our-owner.component.ts
+++ AdvancedDuctCleaningLLC/src/app/pages/meet-our-owner/meet-our-owner.component.ts
@@ -9,10 +9,9 @@ import {Meta} from "@angular/platform-browser";
 export class MeetOurOwnerComponent {
 
   constructor(private readonly meta: Meta) {
-    meta.addTag({
-      name: 'Scott Crull',
-      content: 'Scott Crull',
-      description: 'Click here to learn more about Scott Crull, the Veteran owner of Advanced Duct Cleaning. Scott Crull loves serving the people of Indiana'
-    })
+    meta.updateTag({name: 'title', content: 'Scott Crull | Advanced Duct Cleaning LLC'});
+    meta.updateTag({property: 'og:title', content: 'Scott Crull | Advanced Duct Cleaning LLC'});
+    meta.updateTag({name: 'description', content: 'Click here to learn more about Scott Crull, the Veteran owner of Advanced Duct Cleaning. Scott Crull loves serving the people of Indiana'});
+    meta.updateTag({property: 'og:description', content: 'Click here to learn more about Scott Crull, the Veteran owner of Advanced Duct Cleaning. Scott Crull loves serving the people of Indiana'});
   }
 }
